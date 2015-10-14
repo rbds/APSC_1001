240d98dab65b8cf3559079008b213fc3514e110e
rbds <rschur18@gmail.com>
2015-10-13 20:40:54 -0400

    add HW 3 and 4 in pdf format

--- Homework/HW3_solutions.m
+++ Homework/HW3_solutions.m
@@ -7,15 +7,16 @@
 v1 = [72 63 57 46 12 1];
 v2 = [12 16 18 32 46 1];
 
-
+part_a = meters_to_inches(v1)
+part_b = kg_to_lb(v2)
 
 %% Problem 2
-error = 1; %initialize error
-t = 0;     %initialize caluclated value
-n = 1;     %initialize counter
-while (error > 1e-6)    %set up while loop
-    t = t + 1/(n^2);    %calculate new summation
-    error = pi^2/6 - t; %calculate error
-    n = n+1;            %advance index
+t = randn(1,100);
+sample_avg = mean(t)
+if (sample_avg < 1)
+    disp('actual mean is less than 1')
+elseif (sample_avg > 1)
+    disp('actual mean is greater than 1')
+else 
+    disp('actual mean is equal to 1')
 end
-n-1 %
