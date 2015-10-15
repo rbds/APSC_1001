475901d8a27fe2f36b69eee9670c84d3d549a505
rbds <rschur18@gmail.com>
2015-10-15 11:33:18 -0400

    minor commit

--- Matlab_Handouts/MAE_in_class.m
+++ Matlab_Handouts/MAE_in_class.m
@@ -1,15 +1,17 @@
 x0 = 0;  %[m], Initial Condition
 r = 20; %[m], Reference Input
-dt = .01;   %[s] time step
-T = 5;      %[s] final time
-N = T/dt;   %number of time steps
-t = linspace(0, T, N+1);
-k = 1;      %Gain
+dt = .1;   %[s] time step
+T = 10;      %[s] final time
+%N = T/dt;   %number of time steps
+t = 0:dt:T+dt;
+N = length(t);
+k = 17.5;      %Gain
+%k = 1/dt*1.75;
 x = zeros(1,N); %initialize vector.
 x(1) = x0;
 
 
-for i = 1:N
+for i = 1:N-1
    e = r-x(i); %[m] error
    u = k*e; %[m/s] control input
    
